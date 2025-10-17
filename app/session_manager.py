f317c1bfec7904bd31847f8da4379d34c129e97d
Luis Ignacio Junior <128869475+oluisjr@users.noreply.github.com>
2025-10-17 09:56:16 -0300

    Update session_manager.py

--- app/session_manager.py
+++ app/session_manager.py
@@ -16,7 +16,7 @@ def initialize_session_state():
             'receita_fmt': '123',
             'entry_code': 'Todos',
             'exit_code': 'Todos',
-            'anos': [], 
+            'anos': '2025', 
             'meses': [], 
             'semanas': [], 
             'dias': []
