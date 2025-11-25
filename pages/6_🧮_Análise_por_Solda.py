8b252040c384b944b0b464c8793a285d57208add
Luis Ignacio Junior <128869475+oluisjr@users.noreply.github.com>
2025-11-25 12:55:40 -0300

    Update 6_🧮_Análise_por_Solda.py

--- pages/6_🧮_Análise_por_Solda.py
+++ pages/6_🧮_Análise_por_Solda.py
@@ -9,7 +9,7 @@ import streamlit.components.v1 as components
 if not st.session_state.get('authenticated', False):
     st.error("Acesso negado. Por favor, faça o login na página principal.")
     st.stop()
-
+    st.navigation(inicio)
 
 # ============================================================
 # CONFIGURAÇÕES DA PÁGINA
